ca70fc25c3782c11bbdb162038181c1b9fdfacb1
Sampreet Klair <sampreetklair@gmail.com>
2024-04-21 17:31:21 -0400

    Adding Assignment 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [`./views/**/*.ejs`],
   theme: {
     extend: {},
   },
-  daisyui: {
-    themes: ['coffee'],
-  },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['fantasy'],
+  },
 }
